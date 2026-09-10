e6b4c95b5868eed79ba660e80d407dc61455278a
saha2310 <134713305+saha2310@users.noreply.github.com>
2026-09-10 09:18:34 +0000

    bb

---
+++ furniture-studio/components/favorites/index.ts
@@ -0,0 +1,4 @@
+export { FavoritesProvider, useFavorites } from './FavoritesProvider';
+export { FavoriteButton } from './FavoriteButton';
+export { FavoriteHeaderButton } from './FavoriteHeaderButton';
+export { FavoritesCount } from './FavoritesCount';
